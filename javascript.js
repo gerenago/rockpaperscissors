aa22f1ee99395bf9646d16de75e52dfa9e61bee5
Hannah <hannahxchapman@yahoo.com>
2022-03-24 16:18:31 -0700

    Redo everything

--- javascript.js
+++ javascript.js
@@ -1,31 +1,4 @@
-function playRound () {
-//user inputs variation of rock/paper/scissors
-    userPlay ()
-    computerPlay ()
-    function userPlay () {
-        let userSelection = prompt ("rock, paper, or scissors?");
-        let lowerCaseSelection = userSelection.toLowerCase();
-        //If input is viable, return value, otherwise repeat
-        if (lowerCaseSelection == "rock" || lowerCaseSelection == "paper" || lowerCaseSelection == "scissors") {
-            return lowerCaseSelection
-            }
-            else {
-                userPlay ();
-                
-        }
-        
-    }
 
-    //computer randomly selects rock/paper/scissors
-    function computerPlay () {
-        var gameOptions = [
-            "rock",
-            "paper",
-            "scissors"
-        ];
-        let computerSelection = gameOptions[Math.floor(Math.random()*gameOptions.length)];
-        return computerSelection
-    }
 
     //user value is compared to computer value
     //if value is same, tie
@@ -43,4 +16,3 @@ function playRound () {
         console.log (`You lose! ${computerSelection} beats ${lowerCaseSelection}`)
     }
     //output is Lose/Win/Tie
-}
