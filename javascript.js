39cdab135377f9f20ccbd8354316dbed2086c4f7
Hannah <hannahxchapman@yahoo.com>
2022-03-23 21:56:05 -0700

    Pseudocode

---
+++ javascript.js
@@ -0,0 +1,13 @@
+//user inputs variation of rock/paper/scissors
+//computer randomly selects rock/paper/scissors
+//user value is compared to computer value
+//if value is same, tie
+//if value is different, check type
+//if computer rock, user paper = win
+//if computer rock, user scissors = lose
+//if computer paper, user rock = lose
+//if computer paper, user scissors = win
+//if computer scissors, user rock = win
+//if computer scissors, user paper = lose
+//output is Lose/Win/Tie
+//
