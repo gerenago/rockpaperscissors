7f2f8fdcf73ff248ae6572ba2138cc663f0eb256
Hannah <hannahxchapman@yahoo.com>
2022-03-25 15:59:11 -0700

    keep track of wins

--- javascript.js
+++ javascript.js
@@ -20,6 +20,8 @@ function playRound (computerSelection, lowerCaseSelection) {
     || (computerSelection == "paper" && lowerCaseSelection == "scissors")
     || (computerSelection == "scissors" && lowerCaseSelection == "rock")) {
             console.log (`You win! ${lowerCaseSelection} beats ${computerSelection}!`)
+            //if win, add 1 to score
+            score ++
     }
     // if anything else (not a tie and not win) player loses, print message
     else {
@@ -27,22 +29,31 @@ function playRound (computerSelection, lowerCaseSelection) {
     }
     //output is Lose/Win/Tie
 }
+//keep score
+let score = 0
 //play multiple round game
 function game () {
 //count # rounds played
 //if # less than 5, play another round
     
     for (let i=0; i<5; i++) {
+        //get selection from user
         let userSelection = prompt("rock, paper, or scissors?");
+        //transition to lowercase to compare
         let lowerCaseSelection = userSelection.toLowerCase()
+        //get selection from computer
         let computerSelection = choices[Math.floor(Math.random()*choices.length)];
+        //play the round with above selections
         playRound (computerSelection, lowerCaseSelection)
 
     }
-//if # = 5, end game, report winner
-//count score
-//user #, computer (rounds-user #)
-//if user wins, +1 to userWin
-//if user loses, +0 to userWin
-//if user ties, +0 to userWin
+    //if the user wins more times than the # of rounds - their score, they win
+    if (score > (i + 1 - score)) {
+        console.log ("You won the game!")
+    }
+    //otherwise the computer wins
+    else {
+        console.log ("You lost the game!")
+    }
+
 }
